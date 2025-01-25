da8f0e19883bc97cf733e51f9122255da3b0dc07
TomasManazza80 <tomas.manazza19@gmail.com>
2025-01-25 01:40:24 -0300

    add

--- dbconnection/db.js
+++ dbconnection/db.js
@@ -3,8 +3,14 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
   host: process.env.HOST,
   port: parseInt(process.env.PORT),
-  dialect: process.env.DIALECT, // Agrega esta línea
-  dialectModule: require('pg'), // Agrega esta línea si estás utilizando PostgreSQL
+  dialect: process.env.DIALECT,
+  dialectModule: require('pg'),
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 module.exports = sequelize;
