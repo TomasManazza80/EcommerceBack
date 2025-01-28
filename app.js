997fbc71021681b64eaf047b69cf5a63d03d6c51
Tomás Augusto Manazza <161159185+TomasManazza80@users.noreply.github.com>
2025-01-27 21:03:59 -0300

    Update app.js

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
+app.set("view engine", "ejs");
+
 app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
